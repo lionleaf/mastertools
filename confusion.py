2493bc3bbba349eaf39f212081ded47a46d77a07
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-04-29 10:40:26 -0400

    Add option to skip already calculated numbers

--- confusion.py
+++ confusion.py
@@ -10,6 +10,7 @@ output_dir = os.getcwd() + '/output'
 
 #Generates the files.txt file required for yolo to read the dataset for all datasets in the folder
 def generateFilelists(nr_of_files = 0):
+  print 'generating file lists. nr_of_files=' + str(nr_of_files)
   for dataset in os.listdir(datasets_path):
     nr = 0
     cpath = os.getcwd() + '/' + datasets_path + '/' + dataset
@@ -22,34 +23,41 @@ def generateFilelists(nr_of_files = 0):
         nr += 1
         filelist.write(cpath+'/images/'+filen+'\n')
         if nr_of_files and nr >= nr_of_files:
-          break;
+          break
     filelist.close()
 
-def testWeight(weight_file):
+def testWeight(redo, weight_file):
   for dataset in os.listdir(datasets_path):
     if dataset == '.DS_Store':
-      continue;
+      continue
     dataset_path = os.getcwd() + '/' + datasets_path+'/'+dataset+"/files.txt"
     outfile_name = output_dir+'/'+weight_file.split("/")[-1]+"-"+dataset
+    if(os.path.isfile(outfile_name)):
+      print '\nresult file already exists. Skipping: ' + outfile_name
+      continue
     outfile = open(outfile_name,'a')
+
+    print '\nStarting ' + outfile_name
 
     yolo_process = subprocess.Popen(["./darknet", 'yolo', 'recall', 'cfg/singleclass.cfg', weight_file, dataset_path], cwd=yolo_path, stderr=outfile)
 
     if yolo_process.wait() != 0:
       print "YOLO fail"
     else:
-      print "Completed " + outfile_name
+      print "\nCompleted " + outfile_name
 
     outfile.close()
 
 
-def testAll():
+def testAll(redo):
+  print 'Testing all files! Redo = ' + str(redo)
   for weight in os.listdir(weights_path):
-    testWeight(os.getcwd() + '/' + weights_path + "/" + weight)
+    testWeight(redo, os.getcwd() + '/' + weights_path + "/" + weight)
 
 
 
 def generateMatrix(outfile):
+  print '\ngenerating matrix'
   output = open(outfile, 'a')
 
   for weight in os.listdir(weights_path):
@@ -72,7 +80,7 @@ def generateMatrix(outfile):
   output.write('\n\n\n')
   output.close()
 
-#generateFilelists(1000)
-#testAll()
+generateFilelists()
+testAll(False)
 generateMatrix(output_dir+"/matrix.txt")
 

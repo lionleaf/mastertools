6a8640bc08840b08717f54087e5fae38911735b2
Stian Jensen <me@stianj.com>
2016-05-22 23:44:21 +0200

    Add support for saving precision-recall to disk

--- precision_recall.py
+++ precision_recall.py
@@ -5,14 +5,17 @@ Calculate and plot precision/recall curves
 from sys import argv
 from PIL import Image
 import os
+import json
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from evaluate_detections import detected_objects_in_image
 from loaders import load_ground_truth, load_predicted_boxes
 
+weights_dir = '../weights'
 dataset_dir = '../datasets'
 valid_dir = '../valid'
+prerec_dir = '../prerec'
 
 
 def identifier_from_path(imagepath):
@@ -71,6 +74,10 @@ def calculate_precision_recall(ground_truth, predicted_boxes, files):
     average_precision = metrics.auc(recall_list, precision_list)
     print 'AP:', average_precision
 
+    return recall_list, precision_list, average_precision
+
+
+def plot_graph(recall_list, precision_list):
     plt.clf()
     plt.plot(recall_list, precision_list, label='Precision-Recall curve')
     plt.xlabel('Recall')
@@ -82,9 +89,18 @@ def calculate_precision_recall(ground_truth, predicted_boxes, files):
     plt.show()
 
 
-if __name__ == '__main__':
-    weights_identifier = argv[1]
-    dataset_identifier = argv[2]
+def load_and_calculate_precision_recall(weights_identifier,
+                                        dataset_identifier):
+    prerec_path = (os.getcwd() + '/' +
+                   prerec_dir + '/prerec-' +
+                   weights_identifier + '.weights-' +
+                   dataset_identifier)
+    if os.path.isfile(prerec_path):
+        with open(prerec_path, 'r') as f:
+            data = json.loads(f.read())
+            return (data['recall_list'],
+                    data['precision_list'],
+                    data['average_precision'])
     valid_path = (os.getcwd() + '/' +
                   valid_dir + '/valid-' +
                   weights_identifier + '.weights-' +
@@ -96,4 +112,35 @@ if __name__ == '__main__':
     predicted_boxes = load_predicted_boxes(valid_path)
     with open(dataset_path, 'r') as f:
         files = map(lambda x: x.strip(), f.readlines())
-    calculate_precision_recall(ground_truth, predicted_boxes, files)
+
+    (recall_list,
+     precision_list,
+     average_precision) = calculate_precision_recall(ground_truth,
+                                                     predicted_boxes,
+                                                     files)
+    with open(prerec_path, 'w') as f:
+        f.write(json.dumps({
+            'recall_list': recall_list,
+            'precision_list': precision_list,
+            'average_precision': average_precision,
+        }))
+    return recall_list, precision_list, average_precision
+
+
+if __name__ == '__main__':
+    if len(argv) > 2:
+        (recall_list,
+         precision_list,
+         _) = load_and_calculate_precision_recall(argv[1], argv[2])
+
+        plot_graph(recall_list, precision_list)
+    else:
+        weight_files = sorted(os.listdir(weights_dir))
+        dataset_files = sorted(os.listdir(dataset_dir))
+        for weight in weight_files:
+            if not weight.endswith('.weights'):
+                continue
+            weight, _ = weight.split('.', 1)
+            for dataset in dataset_files:
+                print '### %s - %s ###' % (weight, dataset)
+                load_and_calculate_precision_recall(weight, dataset)
